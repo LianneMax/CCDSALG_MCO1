241fcd1eaada36523898d0b1142b28fb1bae6279
reg <106438124+reg83@users.noreply.github.com>
2024-10-24 12:35:16 +0800

    Create slowsort.c

---
+++ slowsort.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "slowsort.h"
+#define MAX 32768
+
+double polarAngle(Coord p1, Coord p2, Coord anchor)
+{
+  // statement
+	// return value 
+} // not yet done
+
+void selectSort(int n, Coord points[], Coord anchor)
+{
+	int i, j, low;
+	Coord temp;
+	
+	for (i = 0; i < n-1; i++){
+		// unsure
+		for (j = i+1; j < n; j++){
+			// unsure
+			}
+	}	
+
+  // select sort algo  
+	temp = points[i];
+	points[i] = points[low];
+	points[low] = temp;	
+} // not yet done 
