657088d9da60095fd907eb522413273df3bd0c1d
reg <106438124+reg83@users.noreply.github.com>
2024-10-24 12:16:59 +0800

    Create slowsort.h

---
+++ slowsort.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define MAX 37268
+
+
+typedef struct 
+{
+	int x, y;
+} Coord;
+
+
+double polarAngle(Coord p1, Coord p2, Coord anchor);
+void selectSort(int n, Coord points[], Coord anchor);
